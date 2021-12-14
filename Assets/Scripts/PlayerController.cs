88c73ce02687b572fe9f1529a7180e9464d09637
Arthur Ribeiro <arthhur.ribeiro@gmail.com>
2021-12-14 15:04:54 -0300

    Add velocity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float velocity = 3.5f;
     public CharacterController characterController;
 
     private void Update()
@@ -9,6 +10,6 @@ public class PlayerController : MonoBehaviour
         float xAxis = Input.GetAxis("Horizontal");
         float zAxis = Input.GetAxis("Vertical");
 
-        characterController.SimpleMove(new Vector3(xAxis, 0, zAxis));
+        characterController.SimpleMove(new Vector3(xAxis, 0, zAxis) * velocity);
     }
 }
